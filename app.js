5a6469ba239320584c2e1ccab90994f879b68594
Lisa Chandra <liza761c@gmail.com>
2020-01-19 21:38:21 +0000

    added email feature

--- app.js
+++ app.js
@@ -1,13 +1,62 @@
 var express = require("express"),
+	bodyParser	= require("body-parser"),
+	exphbs = require("express-handlebars"),
+	nodemailer = require("nodemailer"),
 	app = express();
 
 app.use(express.static(__dirname + "/public"));
 // app.use(express.static(path.join(__dirname,"public")));
 
-app.set("view engine", "ejs");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-	res.render("events");
+	res.render("events.ejs");
+});
+
+app.get("/register", function(req, res) {
+	// res.send("REGISTER");
+	res.render("contact");
+});
+
+app.post("/send", function(req, res) {
+	console.log("SENT!");
+	
+	// create reusable transporter object using the default SMTP transport
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 587,
+		secure: false, // true for 465, false for other ports
+		// service: 'gmail',
+		auth: {
+			user: '#',
+			pass: '#'
+    	},
+    	tls:{
+    	rejectUnauthorized:false
+    	}
+  	});
+
+    // setup email data with unicode symbols
+	let mailOptions = {
+        from: '"Nodemailer Contact" <liza761c@gmail.com>', // sender address
+        to: 'lisachandra761@gmail.com', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        // console.log('Message sent: %s', info.messageId);   
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+		res.render('contact', {msg:'Email has been sent'});
+    });
 });
 
 app.listen(process.env.PORT || 3000, function() {
